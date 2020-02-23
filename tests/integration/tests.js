9aacf3af7049c0f7a9f60a0c61b9eee9df19fe36
Ross <rosso5792@gmail.com>
2020-02-23 11:52:26 +0000

    refactored some tests to use arrow functions for consistency

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -73,7 +73,7 @@ describe("calculator functionality", function() {
     );
   });
 
-  it("should return as NaN when dividing by zero", function() {
+  it("should return as NaN when dividing by zero", () => {
     running_total = element(by.css("#running_total"));
     element(by.css("#number4")).click();
     element(by.css("#operator_divide")).click();
